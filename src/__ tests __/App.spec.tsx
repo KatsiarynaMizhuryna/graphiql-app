22b05ef7286799e1e93749222cd935b4fe5ffac8
Anzhalika Turlak <293852647@mail.ru>
2024-08-19 21:22:46 +0400

    feat: project setup

---
+++ src/__ tests __/App.spec.tsx
@@ -0,0 +1,12 @@
+import "@testing-library/jest-dom"
+import { render } from "@testing-library/react"
+// import Home from "@/app/page"
+
+test('demo', () => {
+  expect(true).toBe(true)
+})
+
+// test("Renders the main page", () => {
+//   render(<Home />)
+//   expect(true).toBeTruthy()
+// })
