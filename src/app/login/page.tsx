e9c3e1f386cec630985eed6d45bd7277b0f7c6ed
Anzhalika Turlak <293852647@mail.ru>
2024-08-20 22:03:18 +0400

    fix: settings tsconfig

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -3,7 +3,7 @@ export const LoginPage = () => {
     <main className="flex-grow flex flex-col items-center justify-around p-24">
       <h1 className="text-4xl">Sign In</h1>
     </main>
-  )
-}
+  );
+};
 
 export default LoginPage;
--- src/app/registration/page.tsx
+++ src/app/registration/page.tsx
@@ -3,7 +3,7 @@ const RegistrationPage = () => {
     <main className="flex-grow flex flex-col items-center justify-around p-24">
       <h1 className="text-4xl">Sign Up</h1>
     </main>
-  )
-}
+  );
+};
 
 export default RegistrationPage;
