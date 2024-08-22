c4d2ffc2d9067e1b90d7209e96b809268a21180d
Anzhalika Turlak <293852647@mail.ru>
2024-08-22 17:56:30 +0400

    fix: add config next

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const filename = fileURLToPath(import.meta.url);
+const dirname = path.dirname(filename);
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack: (config) => {
+    config.resolve.alias['@'] = path.join(dirname, 'src');
+    return config;
+  }
+};
 
 export default nextConfig;
