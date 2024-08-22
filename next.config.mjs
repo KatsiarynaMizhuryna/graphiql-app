e02766a36216b3ebcc7eb26fc84eead612093640
Anzhalika Turlak <293852647@mail.ru>
2024-08-22 18:23:07 +0400

    fix: add config next

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,8 @@
 import path from 'path';
 import { fileURLToPath } from 'url';
 
-const filename = fileURLToPath(import.meta.url);
-const dirname = path.dirname(filename);
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
