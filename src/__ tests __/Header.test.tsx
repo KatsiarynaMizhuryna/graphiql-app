c1e4dcf531ecd41d9426756cbd9f5e05a4f9bb7b
Katsiaryna Mizhuryna <katsiaryna.mizhuryna@gmail.com>
2024-08-27 12:00:14 +0300

    feat: tests for header updated in accordance with farebase

--- src/__ tests __/Header.test.tsx
+++ src/__ tests __/Header.test.tsx
@@ -10,11 +10,9 @@ jest.mock('firebase/auth', () => ({
   getAuth: jest.fn(() => ({
     currentUser: null,
     signInWithEmailAndPassword: jest.fn(),
-    signOut: jest.fn(),
-  })),
+    signOut: jest.fn()
+  }))
 }));
-
-
 
 const mockPush = jest.fn();
 jest.mock('next/router', () => ({
@@ -22,12 +20,12 @@ jest.mock('next/router', () => ({
     push: mockPush,
     pathname: '/',
     query: {},
-    asPath: '/',
-  }),
+    asPath: '/'
+  })
 }));
 
 describe('Header Component', () => {
-  beforeEach(() => {   
+  beforeEach(() => {
     (useAuthState as jest.Mock).mockReturnValue([null, false, null]);
   });
 
@@ -42,10 +40,14 @@ describe('Header Component', () => {
 
     fireEvent.scroll(window, { target: { scrollY: 200 } });
     const headerElement = screen.getByTestId('header');
-    expect(headerElement).toHaveClass('fixed shadow-lg py-2 backdrop-blur-lg transform translate-y-0 animate-slideDown');
+    expect(headerElement).toHaveClass(
+      'fixed shadow-lg py-2 backdrop-blur-lg transform translate-y-0 animate-slideDown'
+    );
 
     fireEvent.scroll(window, { target: { scrollY: 0 } });
-    expect(headerElement).not.toHaveClass('fixed shadow-lg py-2 backdrop-blur-lg transform translate-y-0 animate-slideDown');
+    expect(headerElement).not.toHaveClass(
+      'fixed shadow-lg py-2 backdrop-blur-lg transform translate-y-0 animate-slideDown'
+    );
   });
 
   test('renders BlockBtnNotLogged when user is not logged in', () => {
@@ -56,22 +58,35 @@ describe('Header Component', () => {
   });
 
   test('renders BlockBtnIsLogged when user is logged in', () => {
-    (useAuthState as jest.Mock).mockReturnValue([{ uid: '123', email: 'user@example.com' }, false, null]);
+    (useAuthState as jest.Mock).mockReturnValue([
+      { uid: '123', email: 'user@example.com' },
+      false,
+      null
+    ]);
     render(<BlockBtnIsLogged />);
     expect(screen.getByText(/Sign OUT/i)).toBeInTheDocument();
-    expect(screen.getByText((content, element) => 
-        content.startsWith('Welcome,') &&
-        element?.querySelector('b')?.textContent === 'user@example.com' &&
-        content.endsWith('!')
-      )).toBeInTheDocument();
+    expect(
+      screen.getByText(
+        (content, element) =>
+          content.startsWith('Welcome,') &&
+          element?.querySelector('b')?.textContent === 'user@example.com' &&
+          content.endsWith('!')
+      )
+    ).toBeInTheDocument();
   });
 
   test('adds and removes the scroll event listener', () => {
     const addEventListenerSpy = jest.spyOn(window, 'addEventListener');
     const removeEventListenerSpy = jest.spyOn(window, 'removeEventListener');
     const { unmount } = render(<Header />);
-    expect(addEventListenerSpy).toHaveBeenCalledWith('scroll', expect.any(Function));
+    expect(addEventListenerSpy).toHaveBeenCalledWith(
+      'scroll',
+      expect.any(Function)
+    );
     unmount();
-    expect(removeEventListenerSpy).toHaveBeenCalledWith('scroll', expect.any(Function));
+    expect(removeEventListenerSpy).toHaveBeenCalledWith(
+      'scroll',
+      expect.any(Function)
+    );
   });
 });
