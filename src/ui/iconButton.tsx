cc78492ac80f3dd7e0ecd95a96270727a1871922
Katsiaryna Mizhuryna <katsiaryna.mizhuryna@gmail.com>
2024-09-10 16:36:14 +0300

    GraphQl-11-prettifySetup: firebase version updated for removing unnecessary logs

--- src/ui/iconButton.tsx
+++ src/ui/iconButton.tsx
@@ -18,7 +18,13 @@ const IconButton: React.FC<IconButtonWithTextProps> = ({
       onMouseLeave={() => setIsHovered(false)}
       className="relative flex items-center space-x-2 p-2 rounded hover:bg-gray-700"
     >
-      <Image src={iconSrc} alt={iconAlt} width={20} height={15} />
+      <Image
+        src={iconSrc}
+        alt={iconAlt}
+        width={20}
+        height={15}
+        className="w-full h-auto"
+      />
       {isHovered && (
         <span className="absolute left-full ml-2 bg-gray-800 text-white text-xs rounded px-2 py-1 whitespace-nowrap">
           {buttonText}
