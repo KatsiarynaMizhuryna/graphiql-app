747bcc4ef175a8d5580fb60a8041892ff44f1076
Katsiaryna Mizhuryna <katsiaryna.mizhuryna@gmail.com>
2024-09-13 18:38:15 +0300

    GraphQl-11-prettifySetup: styles for header were correct

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -33,8 +33,8 @@ export const Header = () => {
     <header
       className={`page-header w-full top-0 z-50 transition-transform duration-300 ease-out ${
         isSticky
-          ? 'fixed shadow-lg py-2 backdrop-blur-lg transform translate-y-0 animate-slideDown'
-          : ''
+          ? 'fixed shadow-lg py-2 backdrop-blur-lg animate-slideDown'
+          : 'relative'
       }`}
       data-testid="header"
     >
